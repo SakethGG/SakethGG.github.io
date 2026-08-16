9fe42862743ff57efd1a06137bae2cc9ae6fef41
Saketh Vegesna <sakethvegesna.online@gmail.com>
2026-08-16 21:54:21 +0530

    Added yoyo blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sakethgg.github.io',
   integrations: [mdx(), sitemap()],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       themes: {
         light: 'github-light',
